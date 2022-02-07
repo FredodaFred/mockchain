ed65eb4fa3ef6c78f4eb91ab7901fed14e1f3a4b
FredodaFred <FredodaFred@gmail.com>
2022-02-06 19:34:34 -0500

    2/3 done

--- index.js
+++ index.js
@@ -18,3 +18,11 @@ app.listen(port, () => {
     // Log that our server is running in the terminal
     console.log(`Server is listening at http://localhost:${port}/`);
 });
+
+// Imports from our class modules
+const Blockchain = require("./src/blockchain");
+
+// Global variables
+global.difficulty = 5; // Difficulty to mine a particular block
+global.blockchain = new Blockchain(); // Our copy of the blockchain
+global.transactions = []; // Our current transactions
